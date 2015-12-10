4bd2caa8e4e32858d2980e1c117061c42486c69a
Phil Bennett <philipobenito@gmail.com>
2015-12-10 12:48:13 +0000

    Restructure decorators

--- src/Store/AbstractStore.php
+++ src/Store/AbstractStore.php
@@ -3,49 +3,12 @@
 namespace Percy\Store;
 
 use InvalidArgumentException;
-use Percy\Decorator\DecoratorInterface;
+use Percy\Decorator\DecoratorTrait;
 use Percy\Entity\Collection;
-use Percy\Entity\EntityInterface;
 
 abstract class AbstractStore implements StoreInterface
 {
-    /**
-     * Iterate collection and apply decorators based on action.
-     *
-     * @param \Percy\Entity\Collection $collection
-     * @param integer                  $action
-     *
-     * @return boolean
-     */
-    protected function decorate(Collection $collection, $action)
-    {
-        foreach ($collection->getIterator() as $entity) {
-            $decorators = $entity->getDecorators($action);
-            array_walk($decorators, [$this, 'invokeDecorators'], [$entity]);
-        }
-    }
-
-    /**
-     * Invoke callable decorator on entity.
-     *
-     * @param string                        $decorator
-     * @param array                         $properties
-     * @param \Percy\Entity\EntityInterface $entity
-     *
-     * @return void
-     */
-    protected function invokeDecorators($decorator, array $properties, EntityInterface $entity)
-    {
-        $decorator = new $decorator;
-
-        if (! $decorator instanceof DecoratorInterface) {
-            throw new InvalidArgumentException(
-                sprintf('(%s) must be an instance of (Percy\Decorator\DecoratorInterface)', get_class($decorator))
-            );
-        }
-
-        $decorator($entity, $properties);
-    }
+    use DecoratorTrait;
 
     /**
      * Iterate collection and validate data.
