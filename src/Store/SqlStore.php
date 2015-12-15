16775b3570df7ec24b021d96a2161ad6e970e3fd
Phil Bennett <philipobenito@gmail.com>
2015-12-15 14:14:20 +0000

    Argument order

--- src/Store/SqlStore.php
+++ src/Store/SqlStore.php
@@ -50,7 +50,7 @@ class SqlStore extends AbstractStore
     {
         $insert = $this->query->newInsert();
         $insert->into($entity->getDataSource());
-        $insert->cols($entity->toArray(false));
+        $insert->cols($entity->toArray([], false));
 
         $this->dbal->perform($insert->getStatement(), $insert->getBindValues());
     }
@@ -85,7 +85,7 @@ class SqlStore extends AbstractStore
     {
         $update = $this->query->newUpdate();
         $update->table($entity->getDataSource());
-        $update->cols($entity->toArray(false));
+        $update->cols($entity->toArray([], false));
         $update->where(sprintf('%s = ?', $entity->getPrimary()), $entity[$entity->getPrimary()]);
 
         $this->dbal->perform($update->getStatement(), $update->getBindValues());
