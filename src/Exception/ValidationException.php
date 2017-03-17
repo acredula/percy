c4ef428b5bb5aa726560b82785dc943535c7d48d
James Baxter <jamesbaxter2@outlook.com>
2017-03-17 21:08:55 +0000

    Added a nice way to retrieve validation failure messages

--- src/Exception/ValidationException.php
+++ src/Exception/ValidationException.php
@@ -6,5 +6,23 @@ use DomainException;
 
 class ValidationException extends DomainException
 {
+    /**
+     * Set failures.
+     *
+     * @param array
+     */
+    public function setFailures($failures)
+    {
+        $this->failures = $failures;
+    }
 
+    /**
+     * Get failures.
+     *
+     * @return array
+     */
+    public function getFailures()
+    {
+        return $this->failures;
+    }
 }
